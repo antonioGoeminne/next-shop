4136956eee90258bc9c7b7d18c528661e407b1e1
antonionicolas___ <antonionicolas3003@gmail.com>
2024-09-07 10:54:45 -0300

    feat: adding embla carrousel

---
+++ components/ui/caroussel/carouselButton.tsx
@@ -0,0 +1,80 @@
+import React, {
+  ComponentPropsWithRef,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { EmblaCarouselType } from "embla-carousel";
+import { Svg } from "@/components/svg";
+import arrowForwardIcon from "@/icons/arrow-forward.svg";
+import arrowLeftIcon from "@/icons/arrow-left.svg";
+
+type UsePrevNextButtonsType = {
+  prevBtnDisabled: boolean;
+  nextBtnDisabled: boolean;
+  onPrevButtonClick: () => void;
+  onNextButtonClick: () => void;
+};
+
+export const usePrevNextButtons = (
+  emblaApi: EmblaCarouselType | undefined,
+  onButtonClick?: (emblaApi: EmblaCarouselType) => void
+): UsePrevNextButtonsType => {
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+  const onPrevButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollPrev();
+    if (onButtonClick) onButtonClick(emblaApi);
+  }, [emblaApi, onButtonClick]);
+
+  const onNextButtonClick = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollNext();
+    if (onButtonClick) onButtonClick(emblaApi);
+  }, [emblaApi, onButtonClick]);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onSelect).on("select", onSelect);
+  }, [emblaApi, onSelect]);
+
+  return {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  };
+};
+
+type PropType = ComponentPropsWithRef<"button">;
+
+export const PrevButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props;
+
+  return (
+    <button className="min-w-5" type="button" {...restProps}>
+      <Svg path={arrowLeftIcon} />
+      {children}
+    </button>
+  );
+};
+
+export const NextButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props;
+
+  return (
+    <button className="min-w-5" type="button" {...restProps}>
+      <Svg path={arrowForwardIcon}></Svg>
+      {children}
+    </button>
+  );
+};
--- features/home/components/wrapper-reviews.tsx
+++ features/home/components/wrapper-reviews.tsx
@@ -1,9 +1,29 @@
+import { integralFont } from "@/app/layout";
+import { Carousel } from "@/components/ui/caroussel/carousel";
 import { ReviewCard } from "@/features/review/components/review-card";
 
 export const WrapperReviews = () => {
+  const slides = [1, 2, 3, 4];
   return (
-    <div className="bg-white">
-      <ReviewCard />
+    <div className="bg-white p-2">
+      <Carousel
+        title={
+          <p
+            className={`${integralFont.className} text-3xl md:text-5xl max-w-64 md:whitespace-nowrap`}
+          >
+            Our happy customers
+          </p>
+        }
+      >
+        {slides.map((index) => (
+          <div
+            className="embla__slide max-w-[350px] md:max-w-[400px]"
+            key={index}
+          >
+            <ReviewCard />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
