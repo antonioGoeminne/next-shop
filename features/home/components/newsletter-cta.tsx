a6b74ecc12380e69990e011db2ae297af208daaf
antonionicolas___ <antonionicolas3003@gmail.com>
2024-09-07 11:57:23 -0300

    feat: adding newsletter component

---
+++ features/home/components/newsletter-cta.tsx
@@ -0,0 +1,26 @@
+import { integralFont } from "@/app/layout";
+import { Svg } from "@/components/svg";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import email_icon from "@/icons/email.svg";
+
+export const NewsletterCta = () => {
+  return (
+    <div className="bg-black rounded-xl py-6 md:py-8 px-8 md:px-12 flex flex-col md:flex-row items-center md:items-start justify-between mx-8 gap-4">
+      <p
+        className={`${integralFont.className} text-3xl md:text-4xl max-w-[551px] text-white`}
+      >
+        STAY UPTO DATE ABOUT OUR LATEST OFFERS
+      </p>
+      <div className="flex flex-col gap-2 items-center w-full max-w-[350px]">
+        <Input
+          startIcon={<Svg path={email_icon} />}
+          placeholder="Enter your email adress"
+        />
+        <Button className="w-full" variant={"white"}>
+          Subscribe to newsletter
+        </Button>
+      </div>
+    </div>
+  );
+};
